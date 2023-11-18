6221acfd93281cc777cec44cbd1bd7098d62940a
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-11-18 18:43:02 -0300

    lifecycle vue

--- 2-projeto-vue/src/App.vue
+++ 2-projeto-vue/src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <PrimeiroComponente/>
+  <div>
+    <PrimeiroComponente/>
+    <LifeCycle/>
+  </div>
+ 
 </template>
 
 <script>
+import LifeCycle from './components/LifeCycle.vue';
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 export default {
   name: "App",
   components:{
-    PrimeiroComponente
+    PrimeiroComponente,
+    LifeCycle
   },
 };
 </script>
---
+++ 2-projeto-vue/src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+/* mostrando algumas funcionalidades do lyfecycle do vue */
+
+
+<template>
+    <h1>Meu nome é: {{nome}}</h1>
+</template>
+
+<script>
+export default {
+    name:"LifeCycleHooks",
+    data(){
+        return{
+            nome: "Ainda não sei",
+        }
+    },
+    created(){
+        setTimeout(()=>{
+            this.nome = "Muryllo"
+        }, 1000)
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.nome = "Amanda"
+        }, 2000)
+        
+    }
+}
+</script>
