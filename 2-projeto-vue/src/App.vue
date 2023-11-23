de910dc92b3b715d149329dbdc1bd581656905a3
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-11-22 21:07:30 -0300

    estudando VUE

--- 2-projeto-vue/src/App.vue
+++ 2-projeto-vue/src/App.vue
@@ -1,6 +1,7 @@
 /* eslint-disable */
 <template>
   <div>
+    <Header/>
     <PrimeiroComponente />
     <LifeCycle />
   </div>
@@ -9,13 +10,25 @@
 <script>
 import LifeCycle from "./components/LifeCycle.vue";
 import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import Header from "./components/HeaderProject.vue"
 
 export default {
   name: "App",
   components: {
+    Header,
     PrimeiroComponente,
     LifeCycle,
   },
 };
 </script>
+
+<style>
+  body{
+    background-color: lightgray;
+    color: black;
+  }
+  a{
+    color: red;
+  }
+</style>
 /* eslint-disable */
---
+++ 2-projeto-vue/src/components/HeaderProject.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <ul>
+      <li>HOME</li>
+      <li>BLOG</li>
+      <li>CONTANTO</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderProject",
+  data() {},
+};
+</script>
+
+<style scoped>
+.header > ul {
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  
+}
+</style>
