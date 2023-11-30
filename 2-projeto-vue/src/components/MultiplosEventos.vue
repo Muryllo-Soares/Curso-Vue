a4175bac551c1eb6c238b87445a30b5723802b66
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-11-30 11:53:45 -0300

    Estudando multiplos eventos com vue

---
+++ 2-projeto-vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1, $event), segundo(txt2, $event)">Ativar Multiplos Eventos</button>
+        <p>{{multiplos_eventos}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "MultiplosEventos",
+  data(){
+
+    return {
+      multiplos_eventos: "",
+      txt1 : "Primeiro evento ativado",
+      txt2 : "Segundo Evento"
+      
+   }
+  },
+  methods:{
+    primeiro(txt){
+      this.multiplos_eventos = txt
+    },
+     segundo(txt){
+    setTimeout(()=> this.multiplos_eventos = txt , 500)
+  } 
+  }
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
